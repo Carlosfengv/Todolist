93e7a6d0b311c50aff7da05b36ac57e05ed08ae4
冯威 <409925519@qq.com>
2018-10-18 22:04:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import Content from "./Content"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <section>
+        <div className="Nav">
+          <ul className="Navgation">
+            <li><button type="button">Home</button></li>
+            <li><button type="button">TdoList</button></li>
+          </ul>
+        </div>
+        <div className="App">
         <div className="card">
           <Header></Header>
           <Content></Content>
         </div>
       </div>
+      </section>
+      
     );
   }
 }
